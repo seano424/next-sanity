a9ea08441592ffd309c76fede4224650c34759c6
Sean OReilly <soreilly424@gmail.com>
2022-11-11 15:13:43 -0600

    updated hero!

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { urlFor } from '../lib/sanity'
+import Image from 'next/image'
 
 interface HeroProps {
   heading?: string
@@ -15,5 +17,24 @@ interface HeroProps {
 }
 
 export default function Hero(props: HeroProps) {
-  return <div>Hero</div>
+  return (
+    <div className="relative">
+      <Image
+        className="w-full"
+        src={urlFor(props.image?.asset).width(1600).height(600).url()}
+        placeholder="blur"
+        blurDataURL={urlFor(props.image?.asset)
+          .width(1600)
+          .height(600)
+          .blur(50)
+          .url()}
+        alt={props.heading ?? 'Hero Image'}
+        height={600}
+        width={1600}
+      />
+      <div className="absolute inset-0 flex justify-center items-center">
+        <h2 className="text-6xl text-white uppercase">{props.heading}</h2>
+      </div>
+    </div>
+  )
 }
